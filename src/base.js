57f84a0d9cd7b0804723f9efadd78dc18aa99b0c
Giorgix <jackosinapsis@gmail.com>
2018-10-14 22:45:47 -0400

    added items to gitignore and removed firebase API key

--- src/base.js
+++ src/base.js
@@ -2,7 +2,7 @@ import Rebase from 're-base';
 import firebase from 'firebase';
 
 const firebaseApp = firebase.initializeApp({
-	apiKey: 'AIzaSyBcxcIDVeYjLWWg6HVXjnPSh-wC6FYaEKY',
+	apiKey: 'APIKEY',
 	authDomain: 'catch-of-the-day-giorgix.firebaseapp.com',
 	databaseURL: 'https://catch-of-the-day-giorgix.firebaseio.com'
 });
